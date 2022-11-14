d6c2f0619c1616d5849732d0e5dc8b392ea66f2d
Ricard Borrell <rickborrell@gmail.com>
2022-11-14 23:48:10 +0100

    Bugs repaired on AccessControl

--- packages/hardhat/contracts/AccessControl.sol
+++ packages/hardhat/contracts/AccessControl.sol
@@ -31,7 +31,7 @@ contract AccessControl is Ownable {
     mapping(address => uint256) public userID;
     UserData[] public user;
     uint256 public nUsers;
-    AccessControlToken token;
+    AccessControlToken public token;
 
     FareCalculator[] public fare;
     uint256[] public fareTimeStamp;
@@ -75,7 +75,7 @@ contract AccessControl is Ownable {
     }
 
     modifier onlyOffchainTime() {
-        require(onchainTime == true, "The onchainTime value should be true");
+        require(onchainTime == false, "The onchainTime value should be true");
         _;
     }
 
@@ -101,9 +101,14 @@ contract AccessControl is Ownable {
      * constructor and users/fares managment
      */
 
-    constructor(address hardwareAddress_, bool onchainTime_) {
+    constructor(
+        address hardwareAddress_,
+        address tokenAddress_,
+        bool onchainTime_
+    ) {
         hardwareAddress = hardwareAddress_;
         onchainTime = onchainTime_;
+        token = AccessControlToken(tokenAddress_);
         nUsers = 0;
     }
 
@@ -125,23 +130,23 @@ contract AccessControl is Ownable {
         tmpUser.autentificator = newAutentificator_;
     }
 
-    function blockUser(address userAddress_)
+    function blockUser(address userAdrdress_)
         public
         onlyOwner
-        onlyActivated(userAddress_)
+        onlyActivated(userAdrdress_)
     {
-        user[userID[msg.sender] - 1].status = Status.BLOCKED;
-        emit userBlocked(userAddress_, 0);
+        user[userID[userAdrdress_] - 1].status = Status.BLOCKED;
+        emit userBlocked(userAdrdress_, 0);
     }
 
-    function unblockUser(address userAdress_)
+    function unblockUser(address userAdrdress_)
         public
         onlyOwner
-        onlyRegistered(userAdress_)
+        onlyRegistered(userAdrdress_)
     {
-        UserData storage tmpUser = user[userID[msg.sender] - 1];
+        UserData storage tmpUser = user[userID[userAdrdress_] - 1];
         require(tmpUser.status == Status.BLOCKED, "The user is not blocked");
-        tmpUser.status = Status.BLOCKED;
+        tmpUser.status = Status.ACTIVATED;
     }
 
     function addFare(address fareAddress_) public onlyOwner {
@@ -313,7 +318,7 @@ contract AccessControl is Ownable {
             if (end_ < minB) {
                 minB = end_;
             }
-            if (minB - maxA > 0) {
+            if (minB > maxA) {
                 sum += minB - maxA;
             }
         }
@@ -352,7 +357,6 @@ contract AccessControl is Ownable {
         uint256 nFares = fare.length;
         uint256 cost = 0;
         for (uint256 i = 0; i < nFares; i++) {
-            //rick
             if (fareTimeStamp[nFares - 1 - i] < start_) {
                 cost = fare[i].evaluate(
                     start_,
@@ -368,21 +372,21 @@ contract AccessControl is Ownable {
         if (cost > 0) {
             UserData storage tmpUser = user[userID[userAddress_] - 1];
             tmpUser.debt += cost;
+            tmpUser.status = Status.BLOCKED;
             uint256 allowance = token.allowance(msg.sender, address(this));
-            uint256 pay = cost;
             if (allowance < cost) {
-                pay = allowance;
+                emit userBlocked(userAddress_, cost - allowance);
+                cost = allowance;
+            }
+            if (cost > 0) {
                 bool sent = token.transferFrom(
                     userAddress_,
                     address(this),
-                    pay
+                    cost
                 );
                 require(sent, "Failed to pay on exit");
-                tmpUser.status = Status.BLOCKED;
-                emit userBlocked(userAddress_, cost - allowance);
-            }
-            if (pay > 0) {
-                tmpUser.debt -= pay;
+                tmpUser.debt -= cost;
+                if (tmpUser.debt == 0) tmpUser.status = Status.ACTIVATED;
             }
         }
     }
