b813ebc7685632b4240841ac4657492a12288069
Ricard Borrell <rickborrell@gmail.com>
2022-11-14 18:38:25 +0100

    First complete version for contracts

--- packages/hardhat/contracts/AccessControl.sol
+++ packages/hardhat/contracts/AccessControl.sol
@@ -2,49 +2,69 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "./FareCalculator.sol";
+import "./AccessControlToken.sol";
 
 /* TO DO LIST;
-    - batched versions
-    - nom als errors
+    - crida a ERC20
+    - pagar deute   
+    - batched versions com future qork
+    - tot el tema de la privadesa ens ho passem pel forro de moment
 */
 contract AccessControl is Ownable {
+    event userBlocked(address indexed userAddress_, uint256 indexed debt);
+    event newFare(address indexed fareAddress_);
+    event debtPayed(address indexed userAdrdress_);
+
     enum Status {
         BLOCKED,
         ACTIVATED
     }
     struct UserData {
         Status status;
-        address userAddress; //rick: do we need this here?
         uint256[] registers;
         uint256 autentificator;
         bool isInside;
         uint256 debt;
     }
 
-    mapping(address => uint256) private userID;
-    UserData[] private user;
-    uint256 private nUsers;
+    mapping(address => uint256) public userID;
+    UserData[] public user;
+    uint256 public nUsers;
+    AccessControlToken token;
 
-    address private hardwareAddress;
-    bool private onchainTime;
+    FareCalculator[] public fare;
+    uint256[] public fareTimeStamp;
 
+    address public hardwareAddress;
+    bool public onchainTime;
+
+    /**
+     * modifiers
+     */
     modifier onlyHardware() {
         require(msg.sender == hardwareAddress, "You're not the hardware");
         _;
     }
 
     modifier onlyRegistered(address userAddress_) {
-        //rick: posar el nom
         require(userID[userAddress_] > 0, "This user is not registered");
         _;
     }
 
     modifier onlyActivated(address userAdress_) {
-        //rick: posar el nom
         require(
             userID[userAdress_] > 0 &&
                 user[userID[userAdress_] - 1].status == Status.ACTIVATED,
             "This user is not activated"
+        );
+        _;
+    }
+
+    modifier onlyIdInRange(address userAddress_) {
+        require(
+            msg.sender == userAddress_ || this.owner() == userAddress_,
+            "The address is not msg.sener or owner"
         );
         _;
     }
@@ -64,7 +84,7 @@ contract AccessControl is Ownable {
         require(
             (tmpUser.registers).length % 2 == 0 && !tmpUser.isInside,
             "The user is not outside"
-        ); //rick, posar l'adreça en el misatge
+        );
         _;
     }
 
@@ -73,9 +93,13 @@ contract AccessControl is Ownable {
         require(
             (tmpUser.registers).length % 2 == 1 && tmpUser.isInside,
             "The user is not inside"
-        ); //rick, posar l'adreça en el misatge
+        );
         _;
     }
+
+    /**
+     * constructor and users/fares managment
+     */
 
     constructor(address hardwareAddress_, bool onchainTime_) {
         hardwareAddress = hardwareAddress_;
@@ -90,7 +114,6 @@ contract AccessControl is Ownable {
         userID[userAddress_] = nUsers;
         UserData storage tmpUser = user[nUsers - 1];
         tmpUser.status = Status.ACTIVATED;
-        tmpUser.userAddress = userAddress_;
         tmpUser.isInside = false;
     }
 
@@ -102,14 +125,35 @@ contract AccessControl is Ownable {
         tmpUser.autentificator = newAutentificator_;
     }
 
-    function blockUser(address userAdress_)
+    function blockUser(address userAddress_)
         public
         onlyOwner
-        onlyActivated(userAdress_)
+        onlyActivated(userAddress_)
     {
         user[userID[msg.sender] - 1].status = Status.BLOCKED;
+        emit userBlocked(userAddress_, 0);
     }
 
+    function unblockUser(address userAdress_)
+        public
+        onlyOwner
+        onlyRegistered(userAdress_)
+    {
+        UserData storage tmpUser = user[userID[msg.sender] - 1];
+        require(tmpUser.status == Status.BLOCKED, "The user is not blocked");
+        tmpUser.status = Status.BLOCKED;
+    }
+
+    function addFare(address fareAddress_) public onlyOwner {
+        //rick: aqui em falta comprobar que implementa la interface
+        fareTimeStamp.push(block.timestamp);
+        fare.push(FareCalculator(fareAddress_));
+        emit newFare(fareAddress_);
+    }
+
+    /**
+     * accesses registry
+     */
     function enter(address userAdress_)
         public
         onlyOnchainTime
@@ -154,7 +198,18 @@ contract AccessControl is Ownable {
     }
 
     /**
-     *  INFO
+     * pay debt
+     */
+    function payDebt() public onlyRegistered(msg.sender) {
+        uint256 debt = user[userID[msg.sender] - 1].debt;
+        bool sent = token.transferFrom(msg.sender, address(this), debt);
+        require(sent, "Failed to pay debt");
+        user[userID[msg.sender] - 1].debt = 0;
+        emit debtPayed(msg.sender);
+    }
+
+    /**
+     * view functions
      */
     function getUserRegisters(address userAddress_)
         public
@@ -162,17 +217,15 @@ contract AccessControl is Ownable {
         onlyRegistered(userAddress_)
         returns (uint256[] memory)
     {
-        require(
-            msg.sender == userAddress_ || this.owner() == userAddress_,
-            "Is not user or owner"
-        );
         return user[userID[userAddress_] - 1].registers;
-        //rick: tb ha de poder mirar el de les tarifes?
     }
 
-    function isInside(
-        address userAddress_ //rick: onlyOwnerOrSender
-    ) public view onlyRegistered(userAddress_) returns (bool) {
+    function isInside(address userAddress_)
+        public
+        view
+        onlyRegistered(userAddress_)
+        returns (bool)
+    {
         return user[userID[msg.sender] - 1].isInside;
     }
 
@@ -182,61 +235,43 @@ contract AccessControl is Ownable {
         onlyRegistered(userAddress_)
         returns (bool)
     {
-        return isInsideID(userID[userAddress_] - 1, timestamp_);
+        return _isInsideID(userID[userAddress_] - 1, timestamp_);
     }
 
-    function isInsideID(uint256 userID_, uint256 timestamp_)
-        internal
-        view
-        returns (bool)
-    {
-        bool isin = false;
-        require(
-            userID_ >= 0 && userID_ < nUsers,
-            "Out of range access to user"
-        );
-        UserData storage tmpUser = user[userID_];
-        uint256 pos = nUsers + 1;
+    function occupancy() public view returns (uint256) {
+        uint256 nInside = 0;
         for (uint256 i = 0; i < nUsers; ++i) {
-            if (timestamp_ < tmpUser.registers[i]) {
-                pos = i;
+            if (_isInsideID(i, block.timestamp)) {
+                nInside += 1;
             }
         }
-        if (pos >= 0 || pos < nUsers) {
-            isin = true;
-        }
-        return isin;
+        return nInside;
     }
 
     function occupancy(uint256 timestamp_) public view returns (uint256) {
-        uint256 nn = 0;
+        uint256 nInside = 0;
         for (uint256 i = 0; i < nUsers; ++i) {
-            if (isInsideID(i, timestamp_)) {
-                nn += 1;
+            if (_isInsideID(i, timestamp_)) {
+                nInside += 1;
             }
         }
-        return nn;
+        return nInside;
     }
 
-    /**
-     * rick, aixo s'ha de programr
-     */
     function occupancy(uint256 start_, uint256 end_)
         public
         view
         returns (uint256)
     {
-        uint256 nn = 0;
-        /*for (uint256 i = 0; i < nUsers; ++i) {
-            if (isInsideID(i, timestamp_)) {
-                nn += 1;
-            }
-        }*/
-        return nn;
+        uint256 sum = 0;
+        for (uint256 i = 0; i < nUsers; ++i) {
+            sum += _timeInside(i, start_, end_);
+        }
+        return sum;
     }
 
     /**
-     *  Register
+     * internals
      */
     function _register(address userAddress_, uint256 timestamp_)
         internal
@@ -246,12 +281,109 @@ contract AccessControl is Ownable {
         UserData storage tmpUser = user[userID[userAddress_] - 1];
         tmpUser.isInside = !tmpUser.isInside;
         tmpUser.registers.push(timestamp_);
-    }
-    // Funcion to clean registers?
-    // Change registers
 
-    //Tarifes:
-    // temps, timestamp entrada, occupancy, dia de la setmana, tarifa qeu cobri depentnt del les hores que porti
-    // tarifa (entrada - sortida, nick) (view)
-    //tarifa com array
+        if (!tmpUser.isInside) {
+            uint256 nRegisters = tmpUser.registers.length;
+            uint256 start_ = tmpUser.registers[nRegisters - 2];
+            uint256 stop_ = tmpUser.registers[nRegisters - 1];
+            _exitAction(userAddress_, start_, stop_, occupancy(start_, stop_));
+        }
+    }
+
+    function _timeInside(
+        uint256 userID_,
+        uint256 start_,
+        uint256 end_
+    ) internal view returns (uint256) {
+        require(
+            userID_ >= 0 && userID_ < nUsers,
+            "Out of range access to user"
+        );
+        uint256 sum = 0;
+        UserData storage tmpUser = user[userID_];
+        uint256 nRegisters = tmpUser.registers.length;
+        uint256 maxA;
+        uint256 minB;
+        for (uint256 i = 0; i < nRegisters; i += 2) {
+            maxA = tmpUser.registers[i];
+            if (start_ > maxA) {
+                maxA = start_;
+            }
+            minB = tmpUser.registers[i + 1];
+            if (end_ < minB) {
+                minB = end_;
+            }
+            if (minB - maxA > 0) {
+                sum += minB - maxA;
+            }
+        }
+        return sum;
+    }
+
+    function _isInsideID(uint256 userID_, uint256 timestamp_)
+        internal
+        view
+        returns (bool)
+    {
+        require(
+            userID_ >= 0 && userID_ < nUsers,
+            "Out of range access to user"
+        );
+        bool isin = false;
+        UserData storage tmpUser = user[userID_];
+        uint256 nRegisters = tmpUser.registers.length;
+        for (uint256 i = 0; i < nRegisters; i += 2) {
+            if (
+                timestamp_ >= tmpUser.registers[i] &&
+                timestamp_ <= tmpUser.registers[i + 1]
+            ) {
+                isin = true;
+            }
+        }
+        return isin;
+    }
+
+    function _exitAction(
+        address userAddress_,
+        uint256 start_,
+        uint256 stop_,
+        uint256 occupancy_
+    ) public {
+        uint256 nFares = fare.length;
+        uint256 cost = 0;
+        for (uint256 i = 0; i < nFares; i++) {
+            //rick
+            if (fareTimeStamp[nFares - 1 - i] < start_) {
+                cost = fare[i].evaluate(
+                    start_,
+                    stop_,
+                    occupancy_,
+                    userAddress_,
+                    address(this)
+                );
+                break;
+            }
+        }
+
+        if (cost > 0) {
+            UserData storage tmpUser = user[userID[userAddress_] - 1];
+            tmpUser.debt += cost;
+            uint256 allowance = token.allowance(msg.sender, address(this));
+            uint256 pay = cost;
+            if (allowance < cost) {
+                pay = allowance;
+                bool sent = token.transferFrom(
+                    userAddress_,
+                    address(this),
+                    pay
+                );
+                require(sent, "Failed to pay on exit");
+                tmpUser.status = Status.BLOCKED;
+                emit userBlocked(userAddress_, cost - allowance);
+            }
+            if (pay > 0) {
+                tmpUser.debt -= pay;
+            }
+        }
+    }
 }
