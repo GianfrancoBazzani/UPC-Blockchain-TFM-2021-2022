0ca3012289c5575faf9c485dfcad5a79fe75a7d6
Josep Chetrit <jchetrit1999@gmail.com>
2022-11-16 16:46:48 +0100

    Create debts page

---
+++ packages/app/pages/debts.jsx
@@ -0,0 +1,18 @@
+import React, { useState, useEffect, Fragment } from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
+import Image from 'next/image';
+import styled from '@emotion/styled';
+import Layout from '../components/layouts/Layout';
+
+
+const Debts = () => {
+    return (
+        <>
+        <Layout/>
+        <h1>Debts</h1>
+        </>
+    )
+}
+
+export default Debts;
